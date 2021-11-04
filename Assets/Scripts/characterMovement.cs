f7f9c67d5861f1cf5f40778f38f0d1413632f6e9
theCommAndrew <amirabile.mail@gmail.com>
2021-11-04 11:33:15 -0500

    updating gitignore

--- Assets/Scripts/characterMovement.cs
+++ Assets/Scripts/characterMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class characterMovement : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
