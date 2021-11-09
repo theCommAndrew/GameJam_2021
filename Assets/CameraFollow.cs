6e4d5ada85f6006a1aa661c1a1175c33c204f985
theCommAndrew <amirabile.mail@gmail.com>
2021-11-08 23:39:41 -0600

    Smoothed out camera movement
    camera follow speed can be set in Unity under Main Camera object

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] private float cameraSpeed;
     private Func<Vector3> getCameraPosition;
 
     public void Setup(Func<Vector3> getCameraPosition){
@@ -16,6 +17,17 @@ public class CameraFollow : MonoBehaviour
     {
         Vector3 cameraPos = getCameraPosition();
         cameraPos.z = transform.position.z;
-        transform.position = cameraPos;
+
+        Vector3 cameraDir = (cameraPos - transform.position).normalized;
+        float distance = Vector3.Distance(cameraPos, transform.position);;
+
+        if(distance > 0)
+        {
+            Vector3 newPosition = transform.position = transform.position + cameraDir * distance * cameraSpeed * Time.deltaTime;
+
+            float distanceAfterMove = Vector3.Distance(newPosition, cameraPos);
+            transform.position = distanceAfterMove > distance ? cameraPos : newPosition;
+        }
+        
     }
 }
