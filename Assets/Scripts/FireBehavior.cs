6bd14be0f6e935d739a8c3ee3d1d223e31e627c9
Justin <jdr1813@yahoo.com>
2021-11-16 21:51:44 -0600

    firewall and random traps

---
+++ Assets/Scripts/FireBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBehavior : MonoBehaviour
+{
+    public Vector3 moveFireUp = new Vector3(0, .001f, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(FireSpreading());
+    }
+
+    public IEnumerator FireSpreading()
+    {
+        this.transform.localScale += moveFireUp;
+        yield return new WaitForSeconds(30f);
+    }
+}
