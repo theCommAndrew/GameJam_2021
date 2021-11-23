1b2772e658240536d84e42cf6112d4c9a3b12795
theCommAndrew <amirabile.mail@gmail.com>
2021-11-22 23:44:41 -0600

    Added portal to final room
    activates on battle end

--- Assets/Scripts/BackendStuff/Teleporter.cs
+++ Assets/Scripts/BackendStuff/Teleporter.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Teleporter : MonoBehaviour
 {
+    public BattleSystem battleSystem;
     private LevelInfo level;
     private void Start() {
         level = GameObject.FindGameObjectWithTag("Rooms").GetComponent<LevelInfo>();
+        gameObject.SetActive(false);
+        battleSystem.OnBattleEnd += activate;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -15,5 +18,9 @@ public class Teleporter : MonoBehaviour
         {
             SceneManager.LoadScene(level.getLevelNumber() + 1);
         }
+    }
+
+    private void activate(object sender, System.EventArgs e){
+        gameObject.SetActive(true);
     }
 }
